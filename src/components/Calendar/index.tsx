caf2cdf845527325d67951d1b5c1a692f2eb39e4
elton <eltonm.prado@gmail.com>
2024-03-22 11:50:14 -0300

    Calendar and availability - Calculating days of the month

--- src/components/Calendar/index.tsx
+++ src/components/Calendar/index.tsx
@@ -8,8 +8,9 @@ import {
   CalendarTitle,
 } from './styles'
 import { getWeekDays } from '@/utils/get-week-days'
-import { useState } from 'react'
+import { useMemo, useState } from 'react'
 import dayjs from 'dayjs'
+import { array } from 'zod'
 
 export function Calendar() {
   const [currentDate, setCurrentDate] = useState(() => {
@@ -32,6 +33,28 @@ export function Calendar() {
 
   const currentMonth = currentDate.format('MMMM')
   const currentYear = currentDate.format('YYYY')
+
+  const calendarWeeks = useMemo(() => {
+    const daysInMonthArray = Array.from({
+      length: currentDate.daysInMonth(),
+    }).map((_, i) => {
+      return currentDate.set('date', i + 1)
+    })
+
+    const firstWeekDay = currentDate.get('day')
+
+    const previousMonthFillArray = Array.from({
+      length: firstWeekDay,
+    })
+      .map((_, i) => {
+        return currentDate.subtract(i + 1, 'day')
+      })
+      .reverse()
+
+    return [...previousMonthFillArray, ...daysInMonthArray]
+  }, [currentDate])
+
+  console.log(calendarWeeks)
 
   return (
     <CalendarContainer>
